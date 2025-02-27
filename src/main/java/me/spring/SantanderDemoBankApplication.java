9c893c34cbbad402b062f944727d40320497b59f
Felipe Moura <felipemoura-cr@hotmail.com>
2025-02-27 20:13:53 -0300

    First Commit

---
+++ src/main/java/me/spring/SantanderDemoBankApplication.java
@@ -0,0 +1,13 @@
+package me.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDemoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDemoBankApplication.class, args);
+	}
+
+}
